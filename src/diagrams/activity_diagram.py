8ca38e44f55ba593e3056cffe0cc6663309d9c1e
Guilherme Deusdará <guibanci@gmail.com>
2021-04-04 21:47:28 -0300

    Adicionando exceções do diagrama de atividades
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>

--- src/diagrams/activity_diagram.py
+++ src/diagrams/activity_diagram.py
@@ -106,6 +106,12 @@ class Node:
         self.has_max_source=False
         self.has_max_target=False
 
+    def add_source_qtd(self):
+        self.source_qtd=self.source_qtd+1
+    
+    def add_target_qtd(self):
+        self.source_qtd=self.target_qtd+1
+
 class StartNode(Node):
     def __init__(self, name):
         super().__init__(name)
@@ -158,12 +164,20 @@ class Transition:
 class ActivityDiagram:
     def __init__(self, diagram):
         self.name=diagram["name"]
+
+        self.has_start_node=False
+        self.has_final_node=False
+
         self.elements=[]
         self.transitions=[]
 
         for element in diagram["ActivityDiagramElements"]:
             if element["type"] == "StartNode":
-                self.elements.append(StartNode(element["name"]))
+              if not self.has_start_node:
+                  self.elements.append(StartNode(element["name"]))
+                  self.has_start_node=True
+              else:
+                  raise Exception("ActivityDiagramRuleException")
 
             if element["type"] == "Activity":
                 node=Activity(element["name"])
@@ -180,15 +194,49 @@ class ActivityDiagram:
             if element["type"] == "FinalNode":
                 node=FinalNode(element["name"])
                 self.elements.append(node)
+                self.has_final_node=True
 
-        # if self.start_node == None:
-        #     raise Exception("ActivityDiagramRuleException")
-        # if len(self.final_node) == 0:
-        #     raise Exception("ActivityDiagramRuleException")
+        if not self.has_start_node:
+            raise Exception("ActivityDiagramRuleException")
+            
+        if not self.has_final_node:
+            raise Exception("ActivityDiagramRuleException")
 
         for transition in diagram["ActivityDiagramTransitions"]:
             node=Transition(transition["name"], transition["prob"], transition["source"], transition["target"])
+
+            # node for source
+            match_node, index = self.search(node.source)
+            if match_node == None:
+                raise Exception("ActivityDiagramRuleException")
+            
+            if match_node.has_max_source and match_node.max_source == match_node.source_qtd:
+                raise Exception("ActivityDiagramRuleException")
+            
+            match_node.add_source_qtd()
+            self.elements[index] = match_node
+
+            # node for target
+            match_node, index = self.search(node.target)
+            if match_node == None:
+                raise Exception("ActivityDiagramRuleException")
+            
+            if match_node.has_max_target and match_node.max_target == match_node.target_qtd:
+                raise Exception("ActivityDiagramRuleException")
+            
+            match_node.add_target_qtd()
+            self.elements[index] = match_node
+
+
+
             self.transitions.append(node)
+
+    def search(self, name):
+        for i in range(len(self.elements)):
+            if self.elements[i].name == name:
+                return self.elements[i], i
+ 
+        return None, 0
 
     def getXml(self):
         
